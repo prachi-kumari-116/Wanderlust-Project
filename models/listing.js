cf7026adc303d07c73fc07c31d96ffcad0a1ca7a
Prachi Kumari <prachi8854@gmail.com>
2026-03-12 07:07:06 +0530

    Initial commit: Express server with MongoDB connection and listing model

---
+++ models/listing.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema= mongoose.Schema;
+
+const listingSchema = new Schema({
+    title:{
+        type: String,
+        required: true,
+    }, 
+    description: String,
+    image: {
+        type: String,
+        default:"https://unsplash.com/photos/a-blurry-photo-of-a-plant-in-the-snow-_VGtRB7Hx50",
+        set: (v) => 
+            v=== "" 
+            ? "https://unsplash.com/photos/a-blurry-photo-of-a-plant-in-the-snow-_VGtRB7Hx50" 
+            : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports= Listing;
+
