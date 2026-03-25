7a3f1eced3eabfba95a60adbe1085732265fe2a9
Prachi Kumari <prachi8854@gmail.com>
2026-03-25 13:41:35 +0530

    add initial index route setup

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const Listing = require("./models/listing.js");
+const path = require ("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -13,6 +14,9 @@ main()
     console.log(err);
 });
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname , "views"));
+
 async function main() {
   await mongoose.connect(MONGO_URL);
 
@@ -21,19 +25,28 @@ async function main() {
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing ({
-       title: "My New Villa",
-       description: "By the beach",
-       price: 1200,
-       location: "Calangute, Goa",
-       country: "India",
-    });
 
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successful testing");
-});
+//INDEX ROUTE
+app.get("/listings", async(req, res) => { 
+  const allListings = await Listing.find({});
+  res.render ("listings/index", {allListings});
+} );
+
+
+
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing ({
+//        title: "My New Villa",
+//        description: "By the beach",
+//        price: 1200,
+//        location: "Calangute, Goa",
+//        country: "India",
+//     });
+
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful testing");
+// });
 
 app.listen(8080, () => {
   console.log(" app listening on port 8080");
